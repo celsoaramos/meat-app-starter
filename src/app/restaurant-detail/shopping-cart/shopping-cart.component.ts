392fd550eed1961fecd4bef7f78c9d1c6dc61ab7
celsoaramos <celsoaramos@gmail.com>
2018-06-24 22:09:52 -0300

    54. Localizando Preços Para a Moeda Brasileira

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -16,7 +16,19 @@ export class ShoppingCartComponent implements OnInit {
     return this.shoppingCartService.items;
   }
 
+  clear() {
+    this.shoppingCartService.clear()
+  }
+
+  removeItem(item: any) {
+    this.shoppingCartService.removeItem(item)
+  }
+
+  addItem(item: any) {
+    this.shoppingCartService.addItem(item)
+  }
+
   total(): number {
-    return this.shoppingCartService.total();
+    return this.shoppingCartService.total()
   }
 }
